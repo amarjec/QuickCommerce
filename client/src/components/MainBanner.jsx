44d400698aba421bd3cea3a995d206f2001d833c
amarjec <ammujec@gmail.com>
2025-06-02 12:56:29 +0530

    bottom banner sectiion

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -1,32 +1,54 @@
-import React from 'react'
-import { assets } from '../assets/assets'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { assets } from '../assets/assets';
+import { Link } from 'react-router-dom';
 
 const MainBanner = () => {
   return (
-    <div className='relative'>
-        <img src={assets.main_banner_bg} alt="banner" className='w-full hidden md:block' />
-        <img src={assets.main_banner_bg_sm} alt="banner" className='w-full md:hidden' />
-        <div className='absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-24 md:pb-0 px-4 md:p1-18 lg:pl-24'>
-            <h1 className='text-3xl md:text-4xl 1g:text-5x1 font-bold text-center md:text-left max-w-72 md:max-w-80 1g:max-w-105 leading-tight 1g:leading-15'>
-                Freshness You Can Trust, Savings You will Love! 
-            </h1>
-        
+    <div className="relative">
+      <img
+        src={assets.main_banner_bg}
+        alt="banner"
+        className="w-full hidden md:block"
+      />
+      <img
+        src={assets.main_banner_bg_sm}
+        alt="banner"
+        className="w-full md:hidden"
+      />
 
-         <div className='flex items-center mt-6 font-medium'>
-            <Link className='group flex items-center gap-2 px-7 md:px-9 py-3  bg-primary hover:bg-primary-dull text-white rounded transition cursor-pointer' to="/products">
-                Shop Now
-                <img src={assets.white_arrow_icon} alt="arrow"  className='md:hidden transition group-focus:translate-x-1'/>
-            </Link>
+      <div className="absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-24 md:pb-0 px-4 md:pl-18 lg:pl-24">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-[420px] leading-tight lg:leading-[60px]">
+          Freshness You Can Trust, Savings You will Love!
+        </h1>
 
-            <Link className='group hidden md:flex items-center gap-2 px-9 py-3 cursor-pointer' to="/products">
-                Explore deals
-                <img src={assets.black_arrow_icon} alt="arrow"  className='transition group-focus:translate-x-1'/>
-            </Link>
-         </div>
+        <div className="flex items-center mt-6 font-medium">
+          <Link
+            to="/products"
+            className="group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull text-white rounded transition cursor-pointer"
+          >
+            Shop Now
+            <img
+              src={assets.white_arrow_icon}
+              alt="arrow"
+              className="md:hidden transition group-focus:translate-x-1"
+            />
+          </Link>
+
+          <Link
+            to="/products"
+            className="group hidden md:flex items-center gap-2 px-9 py-3 cursor-pointer"
+          >
+            Explore deals
+            <img
+              src={assets.black_arrow_icon}
+              alt="arrow"
+              className="transition group-hover:translate-x-1"
+            />
+          </Link>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainBanner
+export default MainBanner;
