bb845a610aa5d54401863c3e761bf8b711c15cee
Amar Agrawal <mac@Amars-MacBook-Air.local>
2025-08-18 19:49:34 +0530

    register user controller

---
+++ server/models/User.js
@@ -0,0 +1,13 @@
+ import mongoose from "mongoose";
+  
+ const userSchema = new mongoose.Schema({
+     name: {type: String, required: true},
+     email: {type: String, required: true, unique: true}, 
+     password: {type: String, required: true, length: { min: 6 } }, // Added length validation
+     cart: { type: Array, default: [] }, // Added cart field
+ }, {minimize: false}); // Disable minimization to keep empty arrays
+ 
+ 
+ const User = mongoose.models.user || mongoose.model("user",userSchema)
+  
+ export default User;
