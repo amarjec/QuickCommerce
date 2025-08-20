2fe4f23d357af1b8d55fe49eb7f81d252adc7fd8
Amar Agrawal <mac@Amars-MacBook-Air.local>
2025-08-20 20:48:25 +0530

    cloudinary setup

--- server/server.js
+++ server/server.js
@@ -1,33 +1,35 @@
 import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import ConnectDB from './configs/mongdb.js';
 import userRouter from './routes/userRoute.js';
 import sellerRouter from './routes/sellerRoute.js';
+import connectCloudinary from './configs/cloudinary.js';
 
-
-dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 const PORT = process.env.PORT || 4000; 
 
-// Ensure the database connection is established
-await ConnectDB(); 
+
+await ConnectDB();  
+await connectCloudinary(); // Connect to Cloudinary
+
 
 const allowedOrigins = ['http://localhost:5173'];
+
 
 // Middleware to parse JSON requests
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-}));
+app.use(cors({ origin: allowedOrigins, credentials: true}));
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to QuickCommerce Server!');
 });
+
 
 app.use('/api/user', userRouter)
 app.use('/api/seller', sellerRouter)
@@ -36,5 +38,5 @@ app.use('/api/seller', sellerRouter)
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
+ 
 export default app;
