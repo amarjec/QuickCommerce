f470363f08f70007b299a14bcb521d567a7078e3
Amar Agrawal <mac@Amars-MacBook-Air.local>
2025-08-20 21:30:43 +0530

    address - model, controller and route

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -33,10 +33,10 @@ export const addProduct = async (req, res) => {
 export const productList = async (req, res) => {
      try {
         const products = await productModel.find({}).sort({ createdAt: -1 });
-        return res.status(200).json({ success: true, products });
+        res.status(200).json({ success: true, products });
     } catch (error) {
         console.error(error.message); // Use console.error for logging errors
-        return res.status(500).json({ success: false, message: "Something went wrong while fetching products." });
+        res.status(500).json({ success: false, message: "Something went wrong while fetching products." });
     }
     
 }
@@ -49,7 +49,7 @@ export const productById = async (req, res) => {
         res.status(200).json({ success: true, product });
     } catch (error) {
         console.log(error.message); // Use console.log for logging errors
-        return res.status(500).json({ success: false, message: "Something went wrong while fetching the product." });
+        res.status(500).json({ success: false, message: "Something went wrong while fetching the product." });
     }
 
 }
@@ -59,10 +59,10 @@ export const changeStock = async (req, res) => {
     try {
         const { id, inStock } = req.body;
         await productModel.findByIdAndUpdate(id, { inStock });
-        return res.status(200).json({ success: true, message: "Product stock updated successfully.", product });
+        res.status(200).json({ success: true, message: "Product stock updated successfully.", product });
     } catch (error) {
         console.log(error.message);
-        return res.status(500).json({ success: false, message: "Something went wrong while updating product stock." });
+        res.status(500).json({ success: false, message: "Something went wrong while updating product stock." });
     }
 
 } 
---
+++ server/routes/addressRoute.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { authUser } from '../middlewares/auth.js';
+import { addAddress, getAddresses} from '../controllers/addressController.js';
+
+const addressRouter = express.Router();
+
+// Route to add a new address
+addressRouter.post('/add', authUser, addAddress);
+
+// Route to get all addresses for a user
+addressRouter.get('/get', authUser, getAddresses);
+
+  
+export default addressRouter;
