e81caa41b6c19696dae261b61f38f0fde0dd503b
Amar Agrawal <mac@Amars-MacBook-Air.local>
2025-08-20 21:30:43 +0530

    address - model, controller and route

---
+++ server/controllers/addressController.js
@@ -0,0 +1,33 @@
+import addressModel from "../models/addressModel.js";
+
+// Add address - api/address/add
+
+export const addAddress = async (req, res) => {
+    try {
+        const { addAddress, userId} = req.body;
+
+        await addressModel.create({ ...address, userId });
+
+        res.status(201).json({ message: "Address added successfully" });
+        
+    } catch (error) {
+        console.error("Error adding address:", error);
+        res.status(500).json({ message: "Failed to add address" });
+    }
+} 
+
+// Get all addresses for a user - api/address/get
+
+export const getAddresses = async (req, res) => {
+    try {
+        const { userId } = req.query;
+
+        const addresses = await addressModel.find({ userId });
+
+        res.status(200).json({success: true, addresses });
+        
+    } catch (error) {
+        console.error("Error fetching addresses:", error);
+        res.status(500).json({ message: "Failed to fetch addresses" });
+    }
+}
