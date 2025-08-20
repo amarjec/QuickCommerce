e2776e37ef2bec2e76eb0c8b02b106502bbfd09c
Amar Agrawal <mac@Amars-MacBook-Air.local>
2025-08-20 22:34:56 +0530

    bug fix, backend complete

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import { authUser } from '../middlewares/auth.js';
+import authUser from '../middlewares/auth.js';
 import { placeOrderCOD, getUserOrders, getAllOrders} from '../controllers/orderController.js';
-import { authSeller } from '../middlewares/authSeller.js';
+import sellerAuth from '../middlewares/authSeller.js';
 
 const orderRouter = express.Router();
 
 orderRouter.post('/cod', authUser, placeOrderCOD);
 orderRouter.post('/user', authUser, getUserOrders);
-orderRouter.post('/seller', authSeller, getAllOrders);
+orderRouter.post('/seller', sellerAuth, getAllOrders);
 
 
 export default orderRouter;
