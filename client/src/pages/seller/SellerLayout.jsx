b10beb7d925f8634e5ce1390a9d08bcfc85867df
Amar Agrawal <mac@Amars-MacBook-Air.local>
2025-08-23 23:32:46 +0530

    seller login & logout api integration

--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { useAppContext } from '../../context/AppContext';
 import { assets } from '../../assets/assets';
 import { Link, NavLink, Outlet } from 'react-router-dom';
+import toast from 'react-hot-toast';
 
 const SellerLayout = () => {
 
-    const {setIsSeller, navigate} = useAppContext();
+    const {setIsSeller, axios, navigate} = useAppContext();
 
 
     
@@ -18,7 +19,17 @@ const SellerLayout = () => {
 
 
     const logout = async () => {
-        setIsSeller(false);
+        try {
+            const { data } = await axios.get('api/seller/logout');
+            if(data.success) {
+                toast.success(data.message);
+                navigate('/');
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
     }
 
 
