e6912aedb27f6d9ded866283bbc8b662a0168497
Amar Agrawal <ammu.jec@gmail.com>
2025-08-26 15:15:50 +0530

    Update mongdb.js

--- server/configs/mongdb.js
+++ server/configs/mongdb.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
         mongoose.connection.on('connected', () =>{
         console.log("Database Connected")
     })
-    await mongoose.connect(`${process.env.MONGO_URI}/QuickCommerce`)
+    await mongoose.connect(`${process.env.MONGO_URI}`)
     } catch (error) {
         console.error("Database connection failed:", error);
     }
